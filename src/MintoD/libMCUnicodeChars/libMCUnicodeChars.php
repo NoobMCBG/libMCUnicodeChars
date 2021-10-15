dfb4bb694bb7cc4a4aacccf27149c805fbfe39cd
MintoD <mintothedev@protonmail.com>
2021-10-15 19:03:15 +0700

    first commit

---
+++ src/MintoD/libMCUnicodeChars/libMCUnicodeChars.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MintoD\libMCUnicodeChars;
+
+class libMCUnicodeChars
+{
+    public static string $FORWARD_ARROW_BUTTON = "";
+    public static string $LEFT_ARROW_BUTTON = "";
+    public static string $BACKWARDS_ARROW_BUTTON = "";
+    public static string $RIGHT_ARROW_BUTTON = "";
+    public static string $JUMP_BUTTON = "";
+    public static string $CROUCH_BUTTON = "";
+    public static string $FLY_UP_BUTTON = "";
+    public static string $FLY_DOWN_BUTTON = "";
+    public static string $CRAFTABLE_BUTTON_ON = "";
+    public static string $CRAFTABLE_BUTTON_OFF = "";
+    public static string $FOOD_ICON = "";
+    public static string $ARMOR_ICON = "";
+    public static string $MINECOIN = "";
+    public static string $CODE_BUILDER_BUTTON = "";
+    public static string $IMMERSIVE_READER_BUTTON = "";
+    public static string $TOKEN = "";
+
+    //TODO: Make a replace function
+}
