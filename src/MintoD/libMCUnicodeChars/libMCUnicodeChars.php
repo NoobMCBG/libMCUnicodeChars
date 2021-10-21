690c8a8f6e0893048baaa15e64c3b10ed0ab4fa1
MintoD <mintothedev@protonmail.com>
2021-10-21 07:11:30 +0700

    Add curly braces

--- src/MintoD/libMCUnicodeChars/libMCUnicodeChars.php
+++ src/MintoD/libMCUnicodeChars/libMCUnicodeChars.php
@@ -6,7 +6,7 @@ namespace MintoD\libMCUnicodeChars;
 
 class libMCUnicodeChars
 {
-    public const CharactersName = ["FORWARD_ARROW_BUTTON", "LEFT_ARROW_BUTTON", "BACKWARDS_ARROW_BUTTON", "RIGHT_ARROW_BUTTON", "JUMP_BUTTON", "CROUCH_BUTTON", "FLY_UP_BUTTON", "FLY_DOWN_BUTTON", "CRAFTABLE_BUTTON_ON", "CRAFTABLE_BUTTON_OFF", "FOOD_ICON", "ARMOR_ICON", "MINECOIN", "CODE_BUILDER_BUTTON", "IMMERSIVE_READER_BUTTON", "TOKEN"];
+    public const CharactersName = ["{FORWARD_ARROW_BUTTON}", "{LEFT_ARROW_BUTTON}", "{BACKWARDS_ARROW_BUTTON}", "{RIGHT_ARROW_BUTTON}", "{JUMP_BUTTON}", "{CROUCH_BUTTON}", "{FLY_UP_BUTTON}", "{FLY_DOWN_BUTTON}", "{CRAFTABLE_BUTTON_ON}", "{CRAFTABLE_BUTTON_OFF}", "{FOOD_ICON}", "{ARMOR_ICON}", "{MINECOIN}", "{CODE_BUILDER_BUTTON}", "{IMMERSIVE_READER_BUTTON}", "{TOKEN}"];
     public const MCUnicodeChars = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
 
     public static function replace(string $str): string
