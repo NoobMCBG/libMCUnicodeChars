d323f1921c7a711d853eb249d33f956f16c9ef14
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-06-13 14:43:52 +0700

    Code improvements
    Applying something I've just learned...

--- src/MintoD/libMCUnicodeChars/libMCUnicodeChars.php
+++ src/MintoD/libMCUnicodeChars/libMCUnicodeChars.php
@@ -6,42 +6,6 @@ namespace MintoD\libMCUnicodeChars;
 
 class libMCUnicodeChars
 {
-    public const CharactersName = [
-        "{FORWARD_ARROW_BUTTON}",   /*  */
-        "{LEFT_ARROW_BUTTON}",      /*  */
-        "{BACKWARDS_ARROW_BUTTON}", /*  */
-        "{RIGHT_ARROW_BUTTON}",     /*  */
-        "{JUMP_BUTTON}",            /*  */
-        "{CROUCH_BUTTON}",          /*  */
-        "{FLY_UP_BUTTON}",          /*  */
-        "{FLY_DOWN_BUTTON}",        /*  */
-        "{CRAFTABLE_BUTTON_ON}",    /*  */
-        "{CRAFTABLE_BUTTON_OFF}",   /*  */
-        "{FOOD_ICON}",              /*  */
-        "{ARMOR_ICON}",             /*  */
-        "{MINECOIN}",               /*  */
-        "{CODE_BUILDER_BUTTON}",    /*  */
-        "{IMMERSIVE_READER_BUTTON}",/*  */
-        "{TOKEN}"                   /*  */
-    ];
-    public const MCUnicodeChars = [
-        "", /* FORWARD_ARROW_BUTTON    */
-        "", /* LEFT_ARROW_BUTTON       */
-        "", /* BACKWARDS_ARROW_BUTTON  */
-        "", /* RIGHT_ARROW_BUTTON      */
-        "", /* JUMP_BUTTON             */
-        "", /* CROUCH_BUTTON           */
-        "", /* FLY_UP_BUTTON           */
-        "", /* FLY_DOWN_BUTTON         */
-        "", /* CRAFTABLE_BUTTON_ON     */
-        "", /* CRAFTABLE_BUTTON_OFF    */
-        "", /* FOOD_ICON               */
-        "", /* ARMOR_ICON              */
-        "", /* MINECOIN                */
-        "", /* CODE_BUILDER_BUTTON     */
-        "", /* IMMERSIVE_READER_BUTTON */
-        ""  /* TOKEN                   */
-    ];
 
     /**
      * Replace the characters in the string with the Minecraft Unicode characters.
@@ -51,6 +15,24 @@ class libMCUnicodeChars
      */
     public static function replace(string $str): string
     {
-        return str_replace(self::CharactersName, self::MCUnicodeChars, $str);
+        $replacements = [
+            "{FORWARD_ARROW_BUTTON}" => "",
+            "{LEFT_ARROW_BUTTON}" => "",
+            "{BACKWARDS_ARROW_BUTTON}" => "",
+            "{RIGHT_ARROW_BUTTON}" => "",
+            "{JUMP_BUTTON}" => "",
+            "{CROUCH_BUTTON}" => "",
+            "{FLY_UP_BUTTON}" => "",
+            "{FLY_DOWN_BUTTON}" => "",
+            "{CRAFTABLE_BUTTON_ON}" => "",
+            "{CRAFTABLE_BUTTON_OFF}" => "",
+            "{FOOD_ICON}" => "",
+            "{ARMOR_ICON}" => "",
+            "{MINECOIN}" => "",
+            "{CODE_BUILDER_BUTTON}" => "",
+            "{IMMERSIVE_READER_BUTTON}" => "",
+            "{TOKEN}" => ""
+        ];
+        return str_replace(array_keys($replacements), $replacements, $str);
     }
 }
